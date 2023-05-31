fbec6e894248476916b46c4c021442972f2681cd
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-31 20:45:42 +0300

    start

--- test1.py
+++ test1.py
@@ -1,14 +1,22 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-g = Service()
-driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
-# driver = webdriver.Chrome()  # Windows
-# driver = webdriver.Firefox()
-# driver = webdriver.Safari()
-# driver = webdriver.Edge()
-base_url = 'https://www.saucedemo.com/'
-driver.get(base_url)
-driver.maximize_window()
+class Test_1():
+    def test_select_product(self):
+        # options = webdriver.ChromeOptions()
+        # options.add_experimental_option("detach", True)
+        # g = Service()
+        # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
+        # driver = webdriver.Chrome()  # Windows
+        # driver = webdriver.Firefox()
+        driver = webdriver.Safari()
+        # driver = webdriver.Edge()
+        base_url = 'https://www.saucedemo.com/'
+        driver.get(base_url)
+        driver.maximize_window()
+        time.sleep(5)
+
+test = Test_1()
+test.test_select_product()
+
