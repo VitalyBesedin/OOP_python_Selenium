dfbcc984027861e4d9cd7b6f0667fafad00f2ce6
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-06-03 20:59:17 +0300

    final first version works^ but it's not enough

--- HW_5_5_Selenium.py
+++ HW_5_5_Selenium.py
@@ -14,8 +14,21 @@ class TestHW:
 
 
         user_list = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user']
+        # options = webdriver.ChromeOptions()
+        # options.add_experimental_option("detach", True)
+        # g = Service()
+        # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
+        # driver = webdriver.Chrome()  # Windows
+        # driver = webdriver.Firefox()
+        # driver = webdriver.Safari()
+        # driver = webdriver.Edge()
+        # base_url = 'https://www.saucedemo.com/'
+        # driver.get(base_url)
+        # driver.maximize_window()
 
         for i in range(len(user_list)):
+
+            print("Start test", i + 1)
             # options = webdriver.ChromeOptions()
             # options.add_experimental_option("detach", True)
             # g = Service()
@@ -27,7 +40,6 @@ class TestHW:
             base_url = 'https://www.saucedemo.com/'
             driver.get(base_url)
             driver.maximize_window()
-            print("Start test", i + 1)
             password_all = "secret_sauce"
             login = LoginPage(driver)
             login.authorization(user_list[i], password_all)
@@ -40,15 +52,14 @@ class TestHW:
                 print("Login Success")
             except TimeoutException as exception: # negative test
                 print("Login Unsuccessful")
+                # driver.refresh()
                 driver.quit()
                 print("Test Success", i + 1)
                 continue
 
             select_product = MainPage(driver)
-            select_product.select_product("//button[@id='add-to-cart-sauce-labs-backpack']")
-            enter_shopping_cart = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='shopping_cart_badge']")))
-            enter_shopping_cart.click()
-            print("Click Enter Shopping Cart")
+            select_product.select_product("#add-to-cart-sauce-labs-backpack")
+            select_product.click_enter_shopping_cart()
 
             success_test = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='title']")))
             value_success_test = success_test.text
@@ -56,6 +67,7 @@ class TestHW:
             assert value_success_test == "Your Cart"
             print("Test Success", i + 1)
             time.sleep(2)
+            # select_product.enter_item_main_menu(3)
             driver.quit()
 
 print("Start Set of Tests")
--- main_page.py
+++ main_page.py
@@ -6,9 +6,32 @@ from selenium.webdriver.support import expected_conditions as EC
 class MainPage:
     def __init__(self, driver):
         self.driver = driver
+        self.main_menu_items = ["","#react-burger-menu-btn","#logout_sidebar_link",""]
 
     def select_product(self, product_link): # choose and add the product to the cart
 
-        select_product = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, product_link)))
+        select_product = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, product_link)))
         select_product.click()
         print("Click Select Product")
+
+    def click_enter_shopping_cart(self):
+        enter_shopping_cart = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='shopping_cart_badge']")))
+        enter_shopping_cart.click()
+        print("Click Enter Shopping Cart")
+
+    def enter_item_main_menu(self, item):
+        """items: 1 - All Items, 2 - About, 3 - Logout, 4 - Reset App State"""
+        menu = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#react-burger-menu-btn")))
+        menu.click()
+        print("Click menu button")
+        link_about = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.main_menu_items[item-1])))
+        link_about.click()
+        print("Click link menu")
+
+
+
+
+
+
+
+
